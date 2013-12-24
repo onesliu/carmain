d40e77081e124316b18ba54bdc3d6b0982c6412c
onesliu@gmail.com <onesliu@gmail.com>
2013-12-24 18:44:08 +0800

    add 3

--- src/com/leonliu/cm/MainActivity.java
+++ src/com/leonliu/cm/MainActivity.java
@@ -1,27 +1,29 @@
 package com.leonliu.cm;
 
-import com.example.carmaintances.R;
+import java.util.Set;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.example.carmaintances.R;
 
 public class MainActivity extends Activity {
 
+	public int REQUEST_ENABLE_BT = 1;
+	Set<BluetoothDevice> pairedDevices;
+	CharSequence abc[];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (mBluetoothAdapter == null) {
-			AlertToast.showAlert(this, "手机无蓝牙部件");
-		}
-		
-		if (mBluetoothAdapter.isEnabled() == false) {
-			mBluetoothAdapter.enable();
-		}
 		
 	}
 
@@ -31,5 +33,40 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	    builder.setTitle(R.string.title_btpopup);
+	           .setItems(R.array.colors_array, new DialogInterface.OnClickListener() {
+	               public void onClick(DialogInterface dialog, int which) {
+	               // The 'which' argument contains the index position
+	               // of the selected item
+	           }
+	    });
+	    return builder.create();
+	}
+	
+	public void onStart() {
+		super.onStart();
+		
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			AlertToast.showAlert(this, getString(R.string.err_nobluetooth));
+		}
+		
+		if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		
+		pairedDevices = mBluetoothAdapter.getBondedDevices();
+		// If there are paired devices
+		if (pairedDevices.size() > 0) {
+		    // Loop through paired devices
+		    for (BluetoothDevice device : pairedDevices) {
+		        // Add the name and address to an array adapter to show in a ListView
+		        //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+		    }
+		}
+	}
 }
