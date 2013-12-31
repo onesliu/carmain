faec8c7a7ebe8f32bcf33262c2a6949276c9f6cc
onesliu@gmail.com <onesliu@gmail.com>
2013-12-31 11:43:13 +0800

    fix toast.

--- src/com/leonliu/cm/AlertToast.java
+++ src/com/leonliu/cm/AlertToast.java
@@ -7,30 +7,14 @@ import android.widget.Toast;
 
 public class AlertToast {
 
-	private static Handler handler = new Handler(Looper.getMainLooper());
 	private static Toast toast = null;
-	private static Object synObj = new Object();
 
-	public static void showAlert(final Context c, final String msg) {
-
-		new Thread(new Runnable() {
-			public void run() {
-				handler.post(new Runnable() {
-					public void run() {
-						synchronized (synObj) {
-							if (toast != null) {
-								toast.cancel();
-								toast.setText(msg);
-								toast.setDuration(Toast.LENGTH_SHORT);
-							} else {
-								toast = Toast.makeText(c, msg,
-										Toast.LENGTH_SHORT);
-							}
-							toast.show();
-						}
-					}
-				});
-			}
-		}).start();
-	}
+	public static void showAlert(Context c, String msg) {
+        if (toast == null) {
+            toast = Toast.makeText(c , msg, Toast.LENGTH_LONG);
+        } else {
+            toast.setText(msg);
+        }
+        toast.show();
+    }
 }
--- src/com/leonliu/cm/MainActivity.java
+++ src/com/leonliu/cm/MainActivity.java
@@ -1,6 +1,7 @@
 package com.leonliu.cm;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import android.app.Activity;
@@ -20,7 +21,7 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
 	public int REQUEST_ENABLE_BT = 1;
-	private ArrayList<String> btlist;
+	private Map<String,String> btmap;
 	private BluetoothDevice selectedBtDev;
 	private BluetoothAdapter mBluetoothAdapter;
 	private Set<BluetoothDevice> pairedDevices;
@@ -29,7 +30,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		btlist = new ArrayList<String>();
+		btmap = new HashMap<String,String>();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		unregisterReceiver(mReceiver);
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
 		// Register the BroadcastReceiver
 		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
 		IntentFilter filter2 = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
@@ -37,12 +53,7 @@ public class MainActivity extends Activity {
 		registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
 		registerReceiver(mReceiver, filter2);
 		registerReceiver(mReceiver, filter3);
-	}
-	
-	@Override
-	protected void onDestroy() {
-		unregisterReceiver(mReceiver);
-		super.onDestroy();
+		super.onResume();
 	}
 
 	@Override
@@ -50,35 +61,6 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-	
-	public Dialog onCreateDialog() {
-		
-		String[] stringArr = new String[btlist.size()];
-		btlist.toArray(stringArr);
-		
-	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	    builder.setTitle(R.string.title_btpopup)
-	           .setItems(stringArr, new DialogInterface.OnClickListener() {
-	               public void onClick(DialogInterface dialog, int which) {
-		               // The 'which' argument contains the index position
-		               // of the selected item
-	            	   if (btlist.size() == which+1) {
-	            		   mBluetoothAdapter.startDiscovery();
-	            	   }
-	            	   else {
-	            		   int i = 0;
-	            		   for (BluetoothDevice device : pairedDevices) {
-	            			   if (i == which) {
-	            				   selectedBtDev = device;
-	            				   AlertToast.showAlert(MainActivity.this, selectedBtDev.getName());
-	            				   break;
-	            			   }
-	            		   }
-	            	   }
-	               }
-	    });
-	    return builder.create();
 	}
 	
 	public void onStart() {
@@ -98,24 +80,68 @@ public class MainActivity extends Activity {
 		}
 	}
 
+	public Dialog CreateBtPairedDialog() {
+		
+		String[] stringArr = new String[btmap.size()+1];
+		btmap.values().toArray(stringArr);
+		stringArr[stringArr.length-1] = getString(R.string.title_btsearch);
+		
+	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	    builder.setTitle(R.string.title_btpopup)
+	    		.setCancelable(false)
+	    		.setItems(stringArr, new DialogInterface.OnClickListener() {
+	               public void onClick(DialogInterface dialog, int which) {
+		               // The 'which' argument contains the index position
+		               // of the selected item
+	            	   if (btmap.size() == which+1) {
+	            		   if (mBluetoothAdapter.isDiscovering() == false)
+	            			   mBluetoothAdapter.startDiscovery();
+	            	   }
+	            	   else {
+	            		   int i = 0;
+	            		   for (BluetoothDevice device : pairedDevices) {
+	            			   if (i == which) {
+	            				   selectedBtDev = device;
+	            				   AlertToast.showAlert(MainActivity.this, selectedBtDev.getName());
+	            				   break;
+	            			   }
+	            		   }
+	            	   }
+	               }
+	    });
+	    return builder.create();
+	}
+	
+	public Dialog CreateBtSearchedDialog() {
+		
+		String[] stringArr = new String[btmap.size()+1];
+		btmap.values().toArray(stringArr);
+		
+	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	    builder.setTitle(R.string.title_btpopup)
+	    		.setCancelable(false)
+	    		.setItems(stringArr, new DialogInterface.OnClickListener() {
+	               public void onClick(DialogInterface dialog, int which) {
+	               }
+	    });
+	    return builder.create();
+	}
+
 	private void showBtDialog(BluetoothAdapter mBluetoothAdapter) {
 		
 		pairedDevices = mBluetoothAdapter.getBondedDevices();
 		// If there are paired devices
 		if (pairedDevices.size() > 0) {
 		    // Loop through paired devices
-			btlist.clear();
+			btmap.clear();
 		    for (BluetoothDevice device : pairedDevices) {
 		        // Add the name and address to an array adapter to show in a ListView
-		    	btlist.add(device.getName());
+		    	btmap.put(device.getAddress(), device.getName());
 		    }
 		}
 		
-		if (btlist.size() > 0) {
-			btlist.add(getString(R.string.title_btsearch));
-			Dialog dlg = onCreateDialog();
-			dlg.show();
-		}
+		Dialog dlg = CreateBtPairedDialog();
+		dlg.show();
 	}
 	
 	public void onActivityResult (int requestCode, int resultCode, Intent data) {
@@ -135,14 +161,14 @@ public class MainActivity extends Activity {
 	            // Get the BluetoothDevice object from the Intent
 	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 	            // Add the name and address to an array adapter to show in a ListView
-	            btlist.add(device.getName());
+	            btmap.put(device.getAddress(), device.getName());
 	        }
 	        else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-	        	btlist.clear();
+	        	btmap.clear();
 	        }
 	        else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-	    		if (btlist.size() > 0) {
-	    			Dialog dlg = onCreateDialog();
+	    		if (btmap.size() > 0) {
+	    			Dialog dlg = CreateBtSearchedDialog();
 	    			dlg.show();
 	    		}
 	    		else  {
